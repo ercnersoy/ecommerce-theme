8106ded1d2ab2bcedf99d0cc1756d570905dc306
Ercan Ersoy <ercanersoy@reklam5.com>
2022-08-12 17:08:15 +0300

    ecommerce theme

---
+++ src/app.js
@@ -0,0 +1,12 @@
+//require("../node_modules/jquery/dist/jquery")
+window.$ = window.jQuery =
+    require("../node_modules/jquery-migrate/dist/jquery-migrate"),
+    require("../node_modules/bootstrap/dist/js/bootstrap"),
+    require("../node_modules/owl.carousel2/dist/owl.carousel"),
+    require("../node_modules/owl.carousel/dist/owl.carousel"),
+    require("../node_modules/aos/dist/aos"),
+    require("../node_modules/jquery.uniform/dist/js/jquery.uniform.standalone"),
+
+    // require("../node_modules/jquery.scrollto/jquery.scrollTo"),
+
+    require("./js/script");
